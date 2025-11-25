0176521a8da060a16bdfa44c30539525572835bc
Mohamad Ilyas Ali <108250794+yaszzzz@users.noreply.github.com>
2025-11-25 22:24:53 +0700

    fix bug gmail input

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -70,7 +70,7 @@ export default function LoginForm() {
           >
             Email
           </label>
-          <div className="relative">
+            <div className="relative">
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
               <Mail className="h-5 w-5 text-gray-400" />
             </div>
@@ -81,13 +81,14 @@ export default function LoginForm() {
               autoComplete="email"
               required
               value={dataForm.email}
-              onChange={(e) =>
-                setDataForm({ ...dataForm, email: e.target.value })
-              }
+              onChange={(e) => {
+              const value = e.target.value.replace(/[^a-zA-Z0-9@_]/g, "");
+              setDataForm({ ...dataForm, email: value });
+              }}
               className="block w-full pl-10 pr-3 py-3 border border-gray-300 rounded-xl text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500 transition-colors"
               placeholder="masukkan email Anda"
             />
-          </div>
+            </div>
         </div>
 
         <div>
